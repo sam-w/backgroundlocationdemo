79209c337a461abb592a4cae2c8ddd68b0c6e243
Sam Warner <sam@bilue.com.au>
2015-04-28 12:45:59 +1000

    Adding app group category on NSUserDefaults

---
+++ NSUserDefaults+WatchGroup.h
@@ -0,0 +1,16 @@
+//
+//  NSUserDefaults+WatchGroup.h
+//  backgroundlocationdemo
+//
+//  Created by Sam Warner on 28/04/2015.
+//  Copyright (c) 2015 samw. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+@interface NSUserDefaults (WatchGroup)
+
++ (instancetype)watchAppGroupDefaults;
+
+@end
